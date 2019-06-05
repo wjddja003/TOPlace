b5e357c34e1afdf624fdb6162be41484e7613d30
wjddja003 <003645@naver.com>
2019-06-05 21:05:38 +0900

    mapajax

--- semiPjt/src/toplace/model/service/DetailSearchService.java
+++ semiPjt/src/toplace/model/service/DetailSearchService.java
@@ -8,10 +8,43 @@ import space.model.vo.Space;
 import toplace.model.dao.DetailSearchDao;
 
 public class DetailSearchService {
-	public ArrayList<Space> detailSearch(String typeArr,int index){
+	public ArrayList<Space> detailSearch(int inputType,int index){
 		Connection conn=JDBCTemplate.getConnection();
+		String typeArr = "";
 		
-		ArrayList<Space> list = new DetailSearchDao().detailSearch(conn,typeArr,index);
+		ArrayList<Space> list = null;
+		if(inputType==1) {
+			for(int i =0;i<23;i++) {
+				if(i==index*2) {
+					typeArr += "1";
+				}else {
+					typeArr += "_";
+				}
+			}
+			list = new DetailSearchDao().placeTypeSearch(conn,typeArr,index);
+		} else if(inputType==2) {
+			for(int i =0;i<23;i++) {
+				if(i==index*2) {
+					typeArr += "1";
+				}else {
+					typeArr += "_";
+				}
+			}
+		}
+		
+		
+		if(list != null) {
+			JDBCTemplate.commit(conn);
+		}else {
+			JDBCTemplate.rollback(conn);
+		}
+		JDBCTemplate.close(conn);
+		return list;
+	}
+	
+	public ArrayList<Space> searchAddress(){
+		Connection conn=JDBCTemplate.getConnection();
+		ArrayList<Space> list =new DetailSearchDao().searchAddress(conn);
 		if(list != null) {
 			JDBCTemplate.commit(conn);
 		}else {
