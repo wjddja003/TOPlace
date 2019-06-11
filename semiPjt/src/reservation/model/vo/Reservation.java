4646da20e800391eed5be29b7a1ffde793ac817d
unuz <unuz94@gmail.com>
2019-06-11 10:57:52 +0900

    예약페이지 완성!! 예약자 요청사항, 연락처 추가
    성윤호(2019-06-11 오전 10:57)

--- semiPjt/src/reservation/model/vo/Reservation.java
+++ semiPjt/src/reservation/model/vo/Reservation.java
@@ -10,9 +10,13 @@ public class Reservation {
 	private String reservationName;
 	private String reservationDay;
 	private String reservationBooker;
+	private String reservationPhone;
+	private String reservationPhone1;
+	private String reservationPhone2;
 	private String reservationTime;
 	private int reservationPerson;
 	private String reservationOption;
+	private String reservationRequest;
 	private String paymentId;
 	private int paymentPrice;
 	private String paymentCard;
@@ -22,8 +26,10 @@ public class Reservation {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public Reservation(int reservationNo, int s_no, int userNo, String reservationName, String reservationDay,
-			String reservationBooker, String reservationTime, int reservationPerson, String reservationOption,
+			String reservationBooker, String reservationPhone, String reservationPhone1, String reservationPhone2,
+			String reservationTime, int reservationPerson, String reservationOption, String reservationRequest,
 			String paymentId, int paymentPrice, String paymentCard) {
 		super();
 		this.reservationNo = reservationNo;
@@ -32,82 +38,142 @@ public class Reservation {
 		this.reservationName = reservationName;
 		this.reservationDay = reservationDay;
 		this.reservationBooker = reservationBooker;
+		this.reservationPhone = reservationPhone;
+		this.reservationPhone1 = reservationPhone1;
+		this.reservationPhone2 = reservationPhone2;
 		this.reservationTime = reservationTime;
 		this.reservationPerson = reservationPerson;
 		this.reservationOption = reservationOption;
+		this.reservationRequest = reservationRequest;
 		this.paymentId = paymentId;
 		this.paymentPrice = paymentPrice;
 		this.paymentCard = paymentCard;
 	}
+
 	public int getReservationNo() {
 		return reservationNo;
 	}
+
 	public void setReservationNo(int reservationNo) {
 		this.reservationNo = reservationNo;
 	}
+
 	public int getS_no() {
 		return S_no;
 	}
+
 	public void setS_no(int s_no) {
 		S_no = s_no;
 	}
+
 	public int getUserNo() {
 		return userNo;
 	}
+
 	public void setUserNo(int userNo) {
 		this.userNo = userNo;
 	}
+
 	public String getReservationName() {
 		return reservationName;
 	}
+
 	public void setReservationName(String reservationName) {
 		this.reservationName = reservationName;
 	}
+
 	public String getReservationDay() {
 		return reservationDay;
 	}
+
 	public void setReservationDay(String reservationDay) {
 		this.reservationDay = reservationDay;
 	}
+
 	public String getReservationBooker() {
 		return reservationBooker;
 	}
+
 	public void setReservationBooker(String reservationBooker) {
 		this.reservationBooker = reservationBooker;
 	}
+
+	public String getReservationPhone() {
+		return reservationPhone;
+	}
+
+	public void setReservationPhone(String reservationPhone) {
+		this.reservationPhone = reservationPhone;
+	}
+
+	public String getReservationPhone1() {
+		return reservationPhone1;
+	}
+
+	public void setReservationPhone1(String reservationPhone1) {
+		this.reservationPhone1 = reservationPhone1;
+	}
+
+	public String getReservationPhone2() {
+		return reservationPhone2;
+	}
+
+	public void setReservationPhone2(String reservationPhone2) {
+		this.reservationPhone2 = reservationPhone2;
+	}
+
 	public String getReservationTime() {
 		return reservationTime;
 	}
+
 	public void setReservationTime(String reservationTime) {
 		this.reservationTime = reservationTime;
 	}
+
 	public int getReservationPerson() {
 		return reservationPerson;
 	}
+
 	public void setReservationPerson(int reservationPerson) {
 		this.reservationPerson = reservationPerson;
 	}
+
 	public String getReservationOption() {
 		return reservationOption;
 	}
+
 	public void setReservationOption(String reservationOption) {
 		this.reservationOption = reservationOption;
 	}
+
+	public String getReservationRequest() {
+		return reservationRequest;
+	}
+
+	public void setReservationRequest(String reservationRequest) {
+		this.reservationRequest = reservationRequest;
+	}
+
 	public String getPaymentId() {
 		return paymentId;
 	}
+
 	public void setPaymentId(String paymentId) {
 		this.paymentId = paymentId;
 	}
+
 	public int getPaymentPrice() {
 		return paymentPrice;
 	}
+
 	public void setPaymentPrice(int paymentPrice) {
 		this.paymentPrice = paymentPrice;
 	}
+
 	public String getPaymentCard() {
 		return paymentCard;
 	}
+
 	public void setPaymentCard(String paymentCard) {
 		this.paymentCard = paymentCard;
 	}
